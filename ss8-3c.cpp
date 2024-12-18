c120ada722df6b739224b710c22d37e01b95d600
MinhNhat161006 <n24dtcn060@student.ptithcm.edu.vn>
2024-12-18 14:44:20 +0700

    Add files via upload

---
+++ ss8-3c.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Nhập số nguyên: ");
+    scanf("%d", &n);
+
+    int arr[n][n];
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            scanf("%d", &arr[i][j]);
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("%d ", arr[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
